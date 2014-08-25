df79da0a0c4307c6f86633743b49e72f48eb8fe6
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-25 14:46:42 -0500

    added a quiz function

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -46,7 +46,7 @@
 			<input type="submit">
 		</p>
 		<p>
-			<label for="sent"><input type="checkbox" id="sent" name="sent" checked>Save a copy of this email to your sent box.</label>
+			<label for="sent"><input type="checkbox" id="sent" name="sent" checked>Save a copy to your sent box.</label>
 		</p>
 	</form>
 
@@ -54,36 +54,40 @@
 	<form action="my_first_form.php" method="POST">
 	<ol>
 		<li>
+<!-- Remember to label the 'names' and and 'values' to make sense to my future self -->
 			<p>Which OS are you using?</p>
-			<label for="question1"><input type="radio" id="question1" name="question1">Windows</label>
-			<label for="question1"><input type="radio" id="question1" name="question1">Mac OS X</label>
-			<label for="question1"><input type="radio" id="question1" name="question1">Linux</label>
-			<label for="question1"><input type="radio" id="question1" name="question1">Other</label>
+			<label for="question1"><input type="radio" name="question1" value="1">Windows</label>
+			<label for="question1"><input type="radio" name="question1" value="2">Mac OS X</label>
+			<label for="question1"><input type="radio" name="question1" value="3">Linux</label>
+			<label for="question1"><input type="radio" name="question1" value="4">Other</label>
 		</li>
 		<li>
 			<p>Question 2</p>
-			<label for="question2"><input type="radio" id="question2" name="question2">Answer1</label>
-			<label for="question2"><input type="radio" id="question2" name="question2">Answer2</label>
-			<label for="question2"><input type="radio" id="question2" name="question2">Answer3</label>
-			<label for="question2"><input type="radio" id="question2" name="question2">Answer4</label>
+			<label for="question2"><input type="radio" name="question2" value="1">Answer1</label>
+			<label for="question2"><input type="radio" name="question2" value="2">Answer2</label>
+			<label for="question2"><input type="radio" name="question2" value="3">Answer3</label>
+			<label for="question2"><input type="radio" name="question2" value="4">Answer4</label>
 		</li>
 		<li>
 			<p>Question 3</p>
-			<label for="question3"><input type="radio" id="question3" name="question3">Answer1</label>
-			<label for="question3"><input type="radio" id="question3" name="question3">Answer2</label>
-			<label for="question3"><input type="radio" id="question3" name="question3">Answer3</label>
-			<label for="question3"><input type="radio" id="question3" name="question3">Answer4</label>
+			<label for="question3"><input type="radio" name="question3" value="1">Answer1</label>
+			<label for="question3"><input type="radio" name="question3" value="2">Answer2</label>
+			<label for="question3"><input type="radio" name="question3" value="3">Answer3</label>
+			<label for="question3"><input type="radio" name="question3" value="4">Answer4</label>
 		</li>		
 		<li>
 			<p>Question 4</p>
-			<label for="question4"><input type="checkbox" id="question4" name="question4">Answer1</label>
-			<label for="question4"><input type="checkbox" id="question4" name="question4">Answer2</label>
-			<label for="question4"><input type="checkbox" id="question4" name="question4">Answer3</label>
-			<label for="question4"><input type="checkbox" id="question4" name="question4">Answer4</label>
+			<label for="question4"><input type="checkbox" name="question4[]" value="1">Answer1</label>
+			<label for="question4"><input type="checkbox" name="question4[]" value="2">Answer2</label>
+			<label for="question4"><input type="checkbox" name="question4[]" value="3">Answer3</label>
+			<label for="question4"><input type="checkbox" name="question4[]" value="4">Answer4</label>
 		</li>
 
 	</ol>
+	<input type="submit">
 	</form>
+
+	<h2>Select Testing</h2>
 
 	</body>
 </html>
