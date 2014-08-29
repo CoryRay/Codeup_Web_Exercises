5ea68b6f0051eb8ed211bd0a158383dc8b192d9d
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-29 09:36:24 -0500

    saved as a php file

---
+++ public/portfolio.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cory's Portfolio</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.theme.min.css">
+	</head>
+	<body>
+<!-- ----------------------Dumb Navbar----------------------------- -->
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="#">Cory?</a>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	  <ul class="nav navbar-nav">
+		<li class="active"><a href="#">Fake Link</a></li>
+		<li><a href="#">Fooled You</a></li>
+		<li class="dropdown">
+		  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown don't work <span class="caret"></span></a>
+		  <ul class="dropdown-menu" role="menu">
+			<li><a href="#">Action</a></li>
+			<li><a href="#">Another action</a></li>
+			<li><a href="#">Something else here</a></li>
+			<li class="divider"></li>
+			<li><a href="#">Separated link</a></li>
+			<li class="divider"></li>
+			<li><a href="#">One more separated link</a></li>
+		  </ul>
+		</li>
+	  </ul>
+	  <form class="navbar-form navbar-left" role="search">
+		<div class="form-group">
+		  <input type="text" class="form-control" placeholder="Search the nothingness">
+		</div>
+		<button type="submit" class="btn btn-default">Submit</button>
+	  </form>
+	  <ul class="nav navbar-nav navbar-right">
+		<li><a href="#">Link</a></li>
+		<li class="dropdown">
+		  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Go away <span class="caret"></span></a>
+		  <ul class="dropdown-menu" role="menu">
+			<li><a href="#">Please</a></li>
+			<li><a href="#">stop it</a></li>
+			<li><a href="#">why</a></li>
+			<li class="divider"></li>
+			<li>window.close();</li>
+		  </ul>
+		</li>
+	  </ul>
+	</div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!-- ------------------------End Dumb Navbar----------------------------- -->
+
+		<div class="container">
+			<div class="jumbotron">
+				<h1>Welcome to my portfolio!</h1>
+				<p>This is where I will showcase all my cool stuff.</p>
+				<p><a class="btn btn-primary btn-lg" role="button">My best button</a></p>
+				<?php $output = shell_exec('/usr/games/fortune'); echo "$output";?>
+			</div>
+		</div>
+
+ <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+	</body>
+</html>
