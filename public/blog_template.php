e144044d31107567c51592ed2e66714bb29f295f
Cory Rodriguez <coreyreylp@gmail.com>
2014-09-06 14:17:49 -0500

    worked on a blog side project

---
+++ public/blog_template.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Blog Home - Cory Rodriguez Template</title>
+
+    <link href="css/sandstone.bootstrap.min.css" rel="stylesheet">
+    <link href="css/blog_template.css" rel="stylesheet">
+
+    <!--                  KEIFERMODE             -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+	<script>!window.jQuery && document.write('<script src="jquery-1.4.3.min.js"><\/script>')</script>
+	<script src="/js/jquery.raptorize.1.0.js"></script>
+	<script type="text/javascript">
+	     $(window).load(function() {
+	          $('.myButton').raptorize();
+	     });
+	</script>
+	<!--                 ENDKEIFERMODE          -->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Cory</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Resume</a>
+                    </li>
+                    <li>
+                        <a href="#">Portfolio</a>
+                    </li>
+                    <li>
+						<button class="myButton">SUMMON SNAKE</button>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <h1 class="page-header">Cory Days<small>Adventures in Cory</small></h1>
+
+				<!-- JSON Blog Post  -->
+
+				
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#">Blog Post Title</a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php">Cory Rodriguez</a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <hr>
+                <img class="img-responsive" src="img/seinfeld1.jpg" alt="">
+                <hr>
+                <p>George: You're sleeping with the maid? Jerry: Yes. George: I've done that. Did you ever eat an ostrich burger. George: What's so great about a mom and pop store? Let me tell you something, if my mom and pop ran a store I wouldn't shop there? Kramer: It's the timeless art of seduction. Elaine: Snapple? Babu's Brother: No, too fruity. Jerry: We’re pathetic, you know that? George: Yeah, like I don’t know that I’m pathetic. George: Why do they make the condom packets so hard to open? Jerry: Probably to give the woman a chance to change her mind. George: Bald men with no jobs and no money who live with their parents don't approach strange women. George [trying to be cool]: I am down. I am totally down. Mark me down. Man on beach: Is anyone here a marine biologist. Jerry: Do the people that work in those little shops at the airport have any idea what the prices are, everywhere else in the world. Puddy: I painted my face. Elaine: You painted your face? Puddy: Yeah. Elaine: Why? Puddy: Well, you know, support the team.</p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+                <!-- Second Blog Post -->
+                <h2>
+                    <a href="#">Blog Post Title</a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php">Cory Rodriguez</a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
+                <hr>
+                <img class="img-responsive" src="img/banner1.jpg" alt="">
+                <hr>
+                <p>Elaine: Have you ever been to the ballet? Jerry: No, but I've seen people on tiptoes. Estelle Costanza (chastising her son for treating his body like an amusement park): Too bad you couldn't do *that* for a living. Susan: It's not a name, it's a number! George: She wants me to dress smart-casual. What is that? Jerry: I don't know, but you don't have it. The Bubble Boy [to Susan]: How 'bout taking your top off. George: I can't carry a pen. I'm afraid I'll puncture my scrotum. George: Back it up, back it up. Beep, beep, beep. Tractor story? Jerry: ’Beep, beep, beep’? What are you doing. George: Oh, it's got cachet, baby! It's got cachet up the yin-yang! Elaine: I was looking at my chart and it said that I was difficult. Why would they write that? Jerry: They've gotten to know you.</p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+                <!-- Third Blog Post -->
+                <h2>
+                    <a href="#">Blog Post Title</a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php">Cory Rodriguez</a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
+                <hr>
+                <img class="img-responsive" src="img/tree.jpg" alt="">
+                <hr>
+                <p>Mickey: It’s 100% cotton, and some wool. Newman: Too many people got their mail. Close to 80%. Nobody’s ever cracked the 50% barrier! Jerry: I do not like the bank. I've heard the expression 'Laughing all the way to the bank'. I have never seen anyone actually doing it.</p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+                <!-- Pager -->
+                <ul class="pager">
+                    <li class="previous">
+                        <a href="#">&larr; Older</a>
+                    </li>
+                    <li class="next">
+                        <a href="#">Newer &rarr;</a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <div class="col-md-4">
+
+                <!-- Blog Search Well -->
+                <div class="well">
+                    <h4>Blog Search</h4>
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                    <!-- /.input-group -->
+                </div>
+
+                <!-- Blog Categories Well -->
+                <div class="well">
+                    <h4>Blog Categories</h4>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Vidya</a>
+                                </li>
+                                <li><a href="#">Television</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Programming</a>
+                                </li>
+                                <li><a href="#">Life Lessons</a>
+                                </li>
+                                <li><a href="#">Work</a>
+                                </li>
+                                <li><a href="#">Education</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.col-lg-6 -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+
+                <!-- Side Widget Well -->
+                <div class="well">
+                    <h4>Dumb Fortune Here</h4>
+						<?php $output = shell_exec('/usr/games/fortune'); echo "$output";?>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Cory &copy; Your Website 2014</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+	<script src ="js/blog_template.js"></script>
+    <script src="bootstrap/js/jquery-1.11.0.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ public/js/blog_template.js
@@ -0,0 +1,17 @@
+	var post = 
+	[
+		{
+		'date': moment().startOf('day').fromNow(),
+		'title',
+		'content',
+		'image'
+		},
+		{
+		'date',
+		'title',
+		'content',
+		'image'
+		}
+	];
+
+console.log(post);
