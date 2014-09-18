ef5b39d3098da3314ee6c33e5a167a92838967d3
Cory Rodriguez <coreyreylp@gmail.com>
2014-09-18 14:06:20 -0500

    made the database data show up in a table

--- public/national_parks.php
+++ public/national_parks.php
@@ -4,9 +4,15 @@
     
     $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    //variable $stmt queries the database with select everything from the nat'l parks
-    $stmt = $dbc->query('SELECT * FROM national_parks');
+    $offset = isset($_GET['offset']) ? intval($_GET['offset']) : 0;
 
+    //variable $stmt queries the database with select everything from the nat'l parks
+    $stmt = $dbc->query("SELECT * 
+                         FROM national_parks LIMIT 4 OFFSET $offset");
+
+    $row = $stmt->fetchall(PDO::FETCH_ASSOC);
+
+/*
     //shows the amount of columns in the table
     echo "Columns: " . $stmt->columnCount() . PHP_EOL;
 
@@ -15,4 +21,39 @@
     while ($row = $stmt->fetch()) {
         print_r($row);
     }
+*/
 
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>National Parks from DB</title>
+        <style>
+        table, td {
+            border:1px black solid;
+        }
+        </style>
+    </head>
+    <body>
+
+        <table>
+            <tr>
+                <td>name</td>
+                <td>location</td>
+                <td>date established</td>
+                <td>area in acres</td>
+            </tr>
+            <?php foreach ($row as $index => $details) : ?>
+            <tr>
+                <?= "<td>$details[name]</td>"; ?>
+                <?= "<td>$details[location]</td>"; ?>
+                <?= "<td>$details[date_established]</td>"; ?>
+                <?= "<td>$details[area_in_acres]</td>"; ?>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+
+
+    </body>
+</html>
